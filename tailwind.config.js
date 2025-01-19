96ecb033793ee4a9eb4bb515fab44fe26fb4fa85
pernillasterner <psterner@hotmail.com>
2025-01-19 11:25:12 +0100

    Add animation and keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,15 @@ export default {
 
     theme: {
         extend: {
+            animation: {
+                'infinite-scroll': 'infinite-scroll 25s linear infinite'
+            },
+            keyframes: {
+                'infinite-scroll': {
+                    from: { transform: 'translateX(0)'},
+                    to: { transform: 'translateX(-100)'}
+                }
+            },
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
